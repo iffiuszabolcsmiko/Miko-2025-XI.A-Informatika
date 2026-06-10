71c9927e8253ef519f1685cf999de5227dea1288
iffiuszabolcsmiko <iffiu.szabolcs@szekelymikokollegium.ro>
2026-06-10 08:26:58 +0300

    Update turtleGraphics.py

--- LEGO/turtleGraphics.py
+++ LEGO/turtleGraphics.py
@@ -3,13 +3,13 @@ import motor_pair
 import color_sensor
 import runloop
 
-motor_pair.pair(motor_pair.PAIR_1, port.A, port.B)
+motor_pair.pair(motor_pair.PAIR_1, port.A, port.C)
 
-STEP_CM = 10
-TURN_SPEED = 25
-DRIVE_SPEED = 300
+STEP_CM = 5
+TURN_SPEED = 100
+DRIVE_SPEED = 360
 TURN_ANGLE = 90
-DEGREES_PER_CM = 360# kalibrálandó!
+DEGREES_PER_CM = 20# kalibrálandó!
 
 # --- MOZGÁS ---
 async def move_forward(cm):
@@ -33,15 +33,15 @@ async def turn(angle):
     motion_sensor.reset_yaw(0)
     await runloop.sleep_ms(50)# hadd álljon be a reset
 
-    target = angle * 10# tilt_angles tizedfokon ad vissza
+    target = angle * 10 # tilt_angles tizedfokon ad vissza
 
     if angle > 0:
-        motor_pair.move(motor_pair.PAIR_1, 100, velocity=TURN_SPEED)
-        while motion_sensor.tilt_angles()[2] < target:# [2] = yaw
+        motor_pair.move(motor_pair.PAIR_1, 100, velocity=-TURN_SPEED)
+        while motion_sensor.tilt_angles()[0] < target:# [2] = yaw
             await runloop.sleep_ms(5)
     else:
-        motor_pair.move(motor_pair.PAIR_1, 100, velocity=-TURN_SPEED)
-        while motion_sensor.tilt_angles()[2] > target:
+        motor_pair.move(motor_pair.PAIR_1, 100, velocity=TURN_SPEED)
+        while motion_sensor.tilt_angles()[0] > target:
             await runloop.sleep_ms(5)
 
     motor_pair.stop(motor_pair.PAIR_1)
@@ -70,7 +70,7 @@ async def execute(commands):
 async def main():
     await execute("D+D+D+D")
     #while True:
-     #   print(motion_sensor.tilt_angles())
-      #  await runloop.sleep_ms(500)
+        #print(motion_sensor.tilt_angles())
+        #await runloop.sleep_ms(500)
 
 runloop.run(main())
